aeede19e9c623e2d5a30eaa896a33c4680025818
wasabikun1704 <sbk283@naver.com>
2023-09-21 10:12:21 +0900

    duckgame2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,18 +9,32 @@ package org.example;
 
 /* 요구사항
 - D1 요구 : 오리를 생성하고 날게하시오.
+- D1 완료 : 클래스와 메서드를 만들어서 처리
+- D2 요구 : 게임의 디테일을 살리기 위해서 `청둥오리`와 `흰오리`를 생성하고 날게하시오.
 */
 
 class Main {
     public static void main(String[] args) {
-        Duck duck = new Duck();
-        duck.fly();
+        청둥오리 a청둥오리 = new 청둥오리();
+        a청둥오리.날다();
+        // 출력 : 오리가 날개로 날아갑니다.
+
+        흰오리 a흰오리 = new 흰오리();
+        a흰오리.날다();
         // 출력 : 오리가 날개로 날아갑니다.
     }
 }
 
-class Duck {
-    void fly() {
+class 오리 {
+    void 날다() {
         System.out.println("오리가 날개로 날아갑니다.");
     }
+}
+
+class 청둥오리 extends 오리 {
+
+}
+
+class 흰오리 extends 오리 {
+
 }
